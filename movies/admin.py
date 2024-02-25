5287eaa8ec502503e2256c854932176c6949e681
Ömer Dilaveroğlu <77552731+omerdilaveroglu@users.noreply.github.com>
2024-02-25 22:07:57 +0300

    first commit

---
+++ movies/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from movies.models import Contact, Genre, Movie, Person, Video
+
+# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title","is_active", "is_home")
+    prepopulated_fields = {'slug':('title',)}
+    list_filter = ('genres','language','is_active','is_home')
+    search_fields = ('title','description',)
+    list_editable = ('is_active','is_home',)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("full_name","gender","duty_type")
+    list_filter = ('gender','duty_type')
+    search_fields = ('first_name','last_name')
+
+admin.site.register(Movie,MovieAdmin)
+admin.site.register(Person,PersonAdmin)
+admin.site.register(Contact)
+admin.site.register(Genre)
+admin.site.register(Video)
